d2cf6e3f427061e81fae76b33dfc9f3c0b21956f
30189028 <surkhab.mundi@ucalgary.ca>
2025-10-20 13:15:59 -0600

    Practicing JavaScript, HTML and CSS. Oct 20 2025. Going through strings, debugging and operators in JavaScript.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let score; 
+score = 20; 
+const maxScore = 100; 
+
+//Valid strings
+let singleQuote = 'this is a valid string';
+let doubleQuote = "this is also a valid string";
+doubleQuote += " bro";
+let fullQuote = singleQuote + "  " + doubleQuote;
